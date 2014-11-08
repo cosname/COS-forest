b70205b7a0b060917d8e5de1ae59e25598ec7826
Yixuan Qiu <yixuan.qiu@cos.name>
2014-11-08 15:04:00 -0500

    simplify header

--- header.php
+++ header.php
@@ -50,19 +50,7 @@
         });
     });
   </script>
-  
-  <?php /* hard-coded. It's bad, but I need to use it. --Yixuan */ ?>
-  <style type="text/css">
-    #cos-logo {
-      width: 100%;
-      text-align: center;
-    }
-    #cos-logo img {
-      width: 90%;
-      max-width: 521px;
-      max-height: 120px;
-    }
-	</style>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -79,9 +67,7 @@
 			<div id="navbar" class="navbar">
 				<nav id="site-navigation" class="navigation main-navigation" role="navigation">
 					<h3 class="menu-toggle"><?php _e( 'Menu', 'twentythirteen' ); ?></h3>
-					<a class="screen-reader-text skip-link" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentythirteen' ); ?>"><?php _e( 'Skip to content', 'twentythirteen' ); ?></a>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-					<?php get_search_form(); ?>
 				</nav><!-- #site-navigation -->
 			</div><!-- #navbar -->
 		</header><!-- #masthead -->
